1a0beb2ea007f75a71b161694270ab6ad46cef97
Yusfi Adilaksa <yadilaksa@gmail.com>
2021-08-11 19:49:44 +0700

    delete fb login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
+import { GoogleOutlined } from '@ant-design/icons'
 import 'firebase/app'
 import { auth } from '../firebase'
 import firebase from 'firebase/app'
@@ -9,19 +9,12 @@ const Login = () => {
   return (
     <div id="login-page">
       <div id="login-card">
-        <h2>Welcome to Rechat!</h2>
+        <h2>Welcome to ReChat!</h2>
         <div
           className="login-button google"
           onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
         >
           <GoogleOutlined /> Sign in With Google
-        </div>
-        <br /><br />
-        <div
-          className="login-button facebook"
-          onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
-        >
-          <FacebookOutlined /> Sign in With Facbook
         </div>
       </div>
     </div>
